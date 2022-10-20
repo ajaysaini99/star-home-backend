cd696e3433fecfbfd972be8e68b35ce94b9fb012
ajaysainijpr99@gmail.com <ajaysainijpr99@gmail.com>
2022-10-20 21:56:32 +0530

    For prod

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-require('./data/init');
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const { append } = require('express/lib/response');
-const { urlencoded } = require('express');
+require("./data/init");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const { append } = require("express/lib/response");
+const { urlencoded } = require("express");
 
-const homeRouter = require('./routes/home');
+const homeRouter = require("./routes/home");
 
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.use(
   morgan(
     ':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'
@@ -18,12 +18,12 @@ app.use(cors());
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
-app.use('/home', homeRouter);
+app.use("/home", homeRouter);
 
-app.listen(3000, error => {
+app.listen(port, (error) => {
   if (error) {
-    console.log('error occured whilte listening to the port >> ', error);
+    console.log("error occured whilte listening to the port >> ", error);
   }
 
-  console.log('server started on http://localhost:3000');
+  console.log(`server started on ${port}`);
 });
